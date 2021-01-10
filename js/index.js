8bd3c928d23945a4e3769045aedcb7d175ae6cff
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-10 21:06:51 +0530

    Fix CSS - background linear gradient and loader

--- js/index.js
+++ js/index.js
@@ -4,29 +4,24 @@ function clickEvent(ques){
   "text": ques
   });
 
+  userReply.classList.toggle('blur');
   var xhr = new XMLHttpRequest(); /*XMLHttpRequest Syntax*/
   xhr.withCredentials = true;
 
   xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    let random=Math.floor(Math.random() * 4) + 1;/*Generate random number in range*/
-    if (JSON.parse(this.responseText).label=="positive"){
-      document.getElementById("minionImg").src="img/"+"positive"+"Minion"+random+".gif"; /* Modify HTML with JS result*/
+    if (this.readyState === this.DONE ) {
       loader.classList.toggle('hide');
+      userReply.classList.toggle('blur');
+      let random=Math.floor(Math.random() * 4) + 1;/*Generate random number in range*/
+      let responseType = JSON.parse(this.responseText).label;
+      document.getElementById("minionImg").src="img/"+responseType+"Minion"+random+".gif"; /* Modify HTML with JS result*/
     }
-    else{
-      document.getElementById("minionImg").src="img/"+"negative"+"Minion"+random+".gif";
-      loader.classList.toggle('hide');
-    }
-  }
   });
 
-  loader.classList.toggle('hide');
   xhr.open("POST", "https://sentiment-analysis4.p.rapidapi.com/reviews");
   xhr.setRequestHeader("x-rapidapi-host", "sentiment-analysis4.p.rapidapi.com");
   xhr.setRequestHeader("x-rapidapi-key", "ed95d6a8d5msh9801e7ace5e9004p11d682jsn8cfbda00cba6");
   xhr.setRequestHeader("content-type", "application/json");
   xhr.setRequestHeader("accept", "application/json");
-
   xhr.send(data);
 }
